82bab7e735f7524c9c0383b250fab2b330b8762e
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-20 21:31:54 +0000

    Delayed_Job
    Support for delay job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215135521) do
+ActiveRecord::Schema.define(:version => 20121220210403) do
 
   create_table "account_plans", :force => true do |t|
     t.string   "label",                                                       :null => false
@@ -63,6 +63,22 @@ ActiveRecord::Schema.define(:version => 20121215135521) do
   add_index "appliances", ["account_id"], :name => "index_appliances_on_account_id"
   add_index "appliances", ["default"], :name => "index_appliances_on_default"
   add_index "appliances", ["phone_directory_id"], :name => "index_appliances_on_phone_directory_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "messages", :force => true do |t|
     t.integer  "ticket_id",                                                                      :null => false
