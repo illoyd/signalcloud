10c8994c304f1cbd6e512eddd1ef5645ccb6e438
Ian Lloyd <ian.w.lloyd@me.com>
2012-12-21 15:55:12 +0000

    Clean up accounts page
    Major cleanup on accounts responder logic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,83 +1,60 @@
 class AccountsController < ApplicationController
+
+  respond_to :html, :json, :xml
+
   # GET /accounts
   # GET /accounts.json
   def index
     @accounts = Account.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @accounts }
-    end
+    respond_with @accounts
   end
 
   # GET /accounts/1
   # GET /accounts/1.json
   def show
-    @account = Account.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @account }
-    end
+    @account = Account.find( params.exist? :id ? params[:id] : params[:account_id] )
+    respond_with @account
   end
 
   # GET /accounts/new
   # GET /accounts/new.json
   def new
     @account = Account.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @account }
-    end
+    respond_with @account
   end
 
   # GET /accounts/1/edit
   def edit
-    @account = Account.find(params[:id])
+    @account = Account.find( params.exist? :id ? params[:id] : params[:account_id] )
+    respond_with @account
   end
 
   # POST /accounts
   # POST /accounts.json
   def create
     @account = Account.new(params[:account])
-
-    respond_to do |format|
-      if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render json: @account, status: :created, location: @account }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    if @account.save
+      flash[:success] = 'Your account has been created.'
     end
+    respond_with @account
   end
 
   # PUT /accounts/1
   # PUT /accounts/1.json
   def update
-    @account = Account.find(params[:id])
-
-    respond_to do |format|
-      if @account.update_attributes(params[:account])
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
-      end
+    @account = Account.find( params.exist? :id ? params[:id] : params[:account_id] )
+    if @account.update_attributes(params[:account])
+      flash[:success] = 'Your account has been updated.'
     end
+    respond_with @account
   end
 
   # DELETE /accounts/1
   # DELETE /accounts/1.json
   def destroy
-    @account = Account.find(params[:id])
+    @account = Account.find( params.exist? :id ? params[:id] : params[:account_id] )
     @account.destroy
-
-    respond_to do |format|
-      format.html { redirect_to accounts_url }
-      format.json { head :no_content }
-    end
+    flash[:notice] = "Successfully destroyed account."
+    respond_with(@account)
   end
 end
