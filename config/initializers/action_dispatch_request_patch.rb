cae1f2067811be9202713f05592c17ca003943ae
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-22 22:49:55 +0000

    Request patch
    Hack to create an 'original_request_url', which only adds query
    parameters when needed… the original method would append posted
    parameters as well!

---
+++ config/initializers/action_dispatch_request_patch.rb
@@ -0,0 +1,22 @@
+module ActionDispatch
+  class Request
+
+    def original_request_url( username=nil, password=nil )
+      # Split fullpath to remove querystring
+      temp_path = url.include?('?') ? url.split('?')[0] : url
+      
+      # Add query string
+      temp_path += '?' + query_parameters.to_hash unless query_parameters.empty?
+      
+      # Add username and password if they are defined
+      unless username.blank?
+        auth_string = username
+        auth_string += ':'+password unless password.blank?
+        temp_path.gsub!( /(https?:\/\/)/, '\1' + auth_string )
+      end
+      
+      return temp_path
+    end
+
+  end
+end
