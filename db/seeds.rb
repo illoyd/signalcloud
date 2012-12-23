afb3a52b9c6a5b8efa6802d0015a48b9e7fd4977
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-23 00:11:46 +0000

    Users
    Add a set of default users

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,15 @@ shared_plan = AccountPlan.create label:'Shared', month: 0, phone_add: 2, call_in
 dedicated_plan = AccountPlan.create label:'Dedicated', month: 500, phone_add: 1, call_in_add: 0.01, sms_in_add: 0.01, sms_out_add: 0.01
 
 # Add master account (me!)
-my_account = Account.create label:'Master Account', account_sid: 'master', auth_token: 'master', account_plan: master_plan
-  my_account.twilio_account_sid = ENV['TWILIO_MASTER_ACCOUNT_SID']
-  my_account.twilio_auth_token = ENV['TWILIO_MASTER_AUTH_TOKEN']
+master_account = Account.new label:'Master Account', account_sid: 'master', auth_token: 'master', account_plan: master_plan
+  master_account.twilio_account_sid = ENV['TWILIO_MASTER_ACCOUNT_SID']
+  master_account.twilio_auth_token = ENV['TWILIO_MASTER_AUTH_TOKEN']
+  master_account.save!
 
-test_account = Account.create label:'Test Account', account_sid: 'test', auth_token: 'test', account_plan: master_plan
+test_account = Account.new label:'Test Account', account_sid: 'test', auth_token: 'test', account_plan: master_plan
   test_account.twilio_account_sid = ENV['TWILIO_TEST_ACCOUNT']
   test_account.twilio_auth_token = ENV['TWILIO_TEST_AUTH_TOKEN']
+  test_account.save!
 
-# 
+# Add users
+test_user = test_account.users.build first_name: 'Jane', last_name: 'Doe', email: 'jane.doe@ticketpleaseapp.com', password: 'password'
