a77051fd82a2e194ca13324d375c422506f5eac4
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-23 13:59:19 +0000

    seconds_to_words
    Introduce 'seconds_to_words' on Numeric to describes a number of
    seconds as, well, words.

---
+++ config/initializers/numeric_patch.rb
@@ -0,0 +1,27 @@
+class Numeric
+  def seconds_to_words
+    # Split parts
+    mm, ss = self.divmod(60)            #=> [4515, 21]
+    hh, mm = mm.divmod(60)           #=> [75, 15]
+    dd, hh = hh.divmod(24)           #=> [3, 3]
+    
+    # Intelligently select segments - ignore 0 segments
+    text = Array.new
+    text << '%d %s' % [dd, 'day'.pluralize(dd)] if dd > 0
+    text << '%d %s' % [hh, 'hour'.pluralize(hh)] if hh > 0
+    text << '%d %s' % [mm, 'minute'.pluralize(mm)] if mm > 0
+    text << '%d %s' % [ss, 'second'.pluralize(ss)] if ss > 0
+    
+    # Pick which general format to reply
+    return case text.size
+      when 0
+        '0 seconds'
+      when 1
+        text.join
+      else
+        interim = [ text.first(-1).join( ', ' ), text.last ]
+        interim.join ', and '
+    end
+    # return "%d days, %d hours, %d minutes, and %d seconds" % [dd, hh, mm, ss]
+  end
+end
