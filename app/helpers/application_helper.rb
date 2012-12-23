1672a22c067ee10e31d8d1d74ffccb90e025a944
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-23 14:36:11 +0000

    New 'dropdown list' helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     render partial: 'layouts/navlist', object: entries
   end
   
+  def dropdown_list( label, entries = [] )
+    render partial: 'layouts/dropdown', object: entries, locals: { label: label }
+  end
+  
   def icon( kind = :blank )
     render partial: 'layouts/icon', object: ICONS.fetch( kind, kind ).to_s
   end
