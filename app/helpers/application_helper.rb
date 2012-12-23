6eac0b27430fe6de986f16b104c1c7275a6d0377
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-23 12:01:52 +0000

    New nav and icon helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
 
+  def navigation_list( entries = [] )
+    render partial: 'layouts/navlist', object: entries
+  end
+  
+  def icon( kind = :blank )
+    render partial: 'layouts/icon', object: ICONS.fetch( kind, kind ).to_s
+  end
 
 end
---
+++ config/initializers/icons.rb
@@ -0,0 +1,11 @@
+ICONS = {
+  # Objects
+  appliances: 'hdd',
+  phone_directories: 'book',
+  phone_numbers: 'phone',
+  
+  # Common actions
+  show: 'search',
+  edit: 'pencil',
+  delete: 'trash'
+}.freeze
