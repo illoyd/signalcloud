78be8d976c2cd5cc07f434dbd8c9088b20103ef6
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-25 14:41:11 +0000

    Phone number controller + view

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -1,83 +1,121 @@
 class PhoneNumbersController < ApplicationController
+
+  respond_to :html
+
   # GET /phone_numbers
   # GET /phone_numbers.json
   def index
-    @phone_numbers = PhoneNumber.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @phone_numbers }
-    end
+    @phone_numbers = current_account.phone_numbers.all
+    respond_with @phone_numbers
   end
 
   # GET /phone_numbers/1
   # GET /phone_numbers/1.json
-  def show
-    @phone_number = PhoneNumber.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @phone_number }
-    end
-  end
+  #def show
+  #  @phone_number = PhoneNumber.find(params[:id])
+  #
+  #  respond_to do |format|
+  #    format.html # show.html.erb
+  #    format.json { render json: @phone_number }
+  #  end
+  #end
 
   # GET /phone_numbers/new
   # GET /phone_numbers/new.json
-  def new
-    @phone_number = PhoneNumber.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @phone_number }
-    end
-  end
+  #def new
+  #  @phone_number = PhoneNumber.new
+  #
+  #  respond_to do |format|
+  #    format.html # new.html.erb
+  #    format.json { render json: @phone_number }
+  #  end
+  #end
 
   # GET /phone_numbers/1/edit
-  def edit
-    @phone_number = PhoneNumber.find(params[:id])
-  end
+  #def edit
+  #  @phone_number = PhoneNumber.find(params[:id])
+  #end
 
   # POST /phone_numbers
   # POST /phone_numbers.json
   def create
-    @phone_number = PhoneNumber.new(params[:phone_number])
 
-    respond_to do |format|
-      if @phone_number.save
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully created.' }
-        format.json { render json: @phone_number, status: :created, location: @phone_number }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
+    # Purchase the number
+    @phone_number = current_account.phone_numbers.build( params )
+    begin
+      # @phone_number.buy
+      flash[:success] = 'Phone number was successfully created.' if @phone_number.save
+      respond_with @phone_number
+
+    rescue Twilio::Rest::RequestError => ex
+      case ex.code
+        when 21452
+          flash[:error] = 'Number not available. (%s)' % [ex.message]
+        else
+          flash[:error] = 'Unknown error! (%s)' % [ex.message]
       end
+      redirect_to search_phone_numbers_path
     end
   end
 
   # PUT /phone_numbers/1
   # PUT /phone_numbers/1.json
-  def update
-    @phone_number = PhoneNumber.find(params[:id])
-
-    respond_to do |format|
-      if @phone_number.update_attributes(params[:phone_number])
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @phone_number.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  @phone_number = PhoneNumber.find(params[:id])
+  #
+  #  respond_to do |format|
+  #    if @phone_number.update_attributes(params[:phone_number])
+  #      format.html { redirect_to @phone_number, notice: 'Phone number was successfully updated.' }
+  #      format.json { head :no_content }
+  #    else
+  #      format.html { render action: "edit" }
+  #      format.json { render json: @phone_number.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /phone_numbers/1
   # DELETE /phone_numbers/1.json
-  def destroy
-    @phone_number = PhoneNumber.find(params[:id])
-    @phone_number.destroy
-
-    respond_to do |format|
-      format.html { redirect_to phone_numbers_url }
-      format.json { head :no_content }
+  #def destroy
+  #  @phone_number = PhoneNumber.find(params[:id])
+  #  @phone_number.destroy
+  #
+  #  respond_to do |format|
+  #    format.html { redirect_to phone_numbers_url }
+  #    format.json { head :no_content }
+  #  end
+  #end
+  
+  def search
+    # Collection of searchable params
+    search_params = params.dup
+    [ :area_code, :contains, :in_region, :in_postal_code, :near_number, :distance ].each { |k| search_params.delete k }
+    
+    # Extract country code from params
+    country_code = params[:country].upcase
+    
+    # Search and reply
+    begin
+      @available_phone_numbers = current_account.twilio_account.available_phone_numbers.get( country_code ).local.list( search_params )
+      respond_with @available_phone_numbers
+    rescue Twilio::REST::RequestError => ex
+      flash.now[:error] = '%s (%s)' % [ ex.message, ex.code ]
+      @available_phone_numbers = []
     end
   end
+
+#   def buy
+#     # Purchase the number
+#     phone_number_to_buy = params[:phone_number]
+#     begin
+#       @available_phone_numbers = current_account.twilio_client.incoming_phone_numbers.create({:phone_number => phone_number_to_buy})
+#     rescue Twilio::Rest::RequestError => ex
+#       if ex.code = 21452
+#         flash[:error] = 'Number not available. (%s)' % [ex.message]
+#       else
+#         flash[:error] = 'Unknown error! (%s)' % [ex.message] 
+#       end
+#     end
+#   end
+
 end
--- test/unit/phone_number_test.rb
+++ test/unit/phone_number_test.rb
@@ -4,4 +4,112 @@ class PhoneNumberTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  UNAVAILABLE_NUMBER = '+15005550000'
+  INVALID_NUMBER = '+15005550001'
+  AVAILABLE_NUMBER = '+15005550006'
+  
+  UNAVAILABLE_AREACODE = '533'
+  AVAILABLE_AREACODE = '500'
+  
+  test "should save phone number" do
+    # Build a basic phone number and save
+    account = accounts( :test_account )
+    phone_number = account.phone_numbers.build( { number: AVAILABLE_NUMBER } )
+    phone_number.twilio_phone_number_sid = 'TEMP'
+    assert phone_number.valid?
+    
+    # Save number
+    assert_difference( 'account.phone_numbers.count' ) do
+      assert phone_number.save, "Failed to save phone_number"
+    end
+  end
+  
+  test "should not save phone number" do
+    # Get test account
+    account = accounts( :test_account )
+    
+    # Build a new generic phone number missing some details
+    phone_number = account.phone_numbers.build( { number: nil } )
+    assert !phone_number.valid?
+    
+    phone_number.number = AVAILABLE_NUMBER
+    assert !phone_number.valid?
+    
+    phone_number.twilio_phone_number_sid = 'TEMP'
+    assert !phone_number.valid?
+    
+    # Save number (and fail)
+    assert_no_difference( 'account.phone_numbers.count' ) do
+      assert !phone_number.save, "Saved phone_number when model was invalid"
+    end
+  end
+  
+  test "should not save (missing account) phone number" do
+    # Get test account
+    account = accounts( :test_account )
+    
+    # Build a new generic phone number missing some details
+    phone_number = PhoneNumber.new( { number: nil } )
+    assert !phone_number.valid?
+    
+    # Save number (and fail)
+    assert_no_difference( 'account.phone_numbers.count' ) do
+      assert !phone_number.save, "Saved phone_number when model was invalid"
+    end
+
+    phone_number.number = AVAILABLE_NUMBER
+    assert !phone_number.valid?
+    
+    # Save number (and fail)
+    assert_no_difference( 'account.phone_numbers.count' ) do
+      assert !phone_number.save, "Saved phone_number when model was invalid"
+    end
+
+    phone_number.twilio_phone_number_sid = 'TEMP'
+    assert !phone_number.valid?
+    
+    # Save number (and fail)
+    assert_no_difference( 'account.phone_numbers.count' ) do
+      assert !phone_number.save, "Saved phone_number when model was invalid"
+    end
+
+    phone_number.account = accounts( :test_account )
+    
+    # Save number (and fail)
+    assert_no_difference( 'account.phone_numbers.count' ) do
+      assert !phone_number.save, "Saved phone_number when model was invalid"
+    end
+  end
+  
+  test 'buy available phone number' do
+    account = accounts( :test_account )
+    phone_number = account.phone_numbers.new( { number: AVAILABLE_NUMBER } )
+    assert_nil phone_number.twilio_phone_number_sid
+
+    # Make purchase
+    assert_nothing_raised( Twilio::REST::RequestError ) { phone_number.buy }
+    assert_not_nil phone_number.twilio_phone_number_sid
+  end
+  
+  test 'cannot buy invalid phone number' do
+    account = accounts( :test_account )
+    phone_number = account.phone_numbers.new( { number: INVALID_NUMBER } )
+    assert_nil phone_number.twilio_phone_number_sid
+
+    # Make purchase
+    assert_raise( Twilio::REST::RequestError ) { phone_number.buy }
+    assert_nil phone_number.twilio_phone_number_sid
+  end
+  
+  test 'cannot buy unavailable phone number' do
+    account = accounts( :test_account )
+    phone_number = account.phone_numbers.new( { number: UNAVAILABLE_NUMBER } )
+    assert_nil phone_number.twilio_phone_number_sid
+
+    # Make purchase
+    assert_raise( Twilio::REST::RequestError ) { phone_number.buy }
+    assert_nil phone_number.twilio_phone_number_sid
+  end
+  
 end
