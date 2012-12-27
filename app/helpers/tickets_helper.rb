802e9b3305f7abc2caa1f4f723053b1cd29af32d
Ian Lloyd <ian.w.lloyd@me.com>
2012-12-27 12:53:23 +0000

    Updating UI
    Update the ticket management user interface

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,15 @@
 module TicketsHelper
+ 
+  def appliance_dropdown_list( current_account, current_appliance )
+    # Pick the dropdown list title
+    title = 'Current Filter: ' + ( current_appliance.nil? ? 'All' : current_appliance.label )
+    
+    # Assemble the entries based upon the current
+    entries = [ { label: 'Show all tickets', icon: 'ok-circle', link: tickets_path } ]
+    current_account.appliances( order: :label ).each do |appliance|
+      entries << { label: 'Filter by ' + appliance.label, icon: :appliances, link: appliance_tickets_path( appliance ) }
+    end
+    
+    dropdown_list( title, entries )
+  end
 end
