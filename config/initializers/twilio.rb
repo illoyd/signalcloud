f6d55246895cab08ad0676b475daa4c11c8ea6fa
Ian Lloyd <ian.w.lloyd@gmail.com>
2012-12-29 11:17:44 +0000

    Merge Twilio initializers
    Move Twilio monkey-patches into central 'twilio' initializer library.

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,3 +1,17 @@
 # Twilio API items
-
 TWILIO_SID_LENGTH = 34
+
+##
+# Patch for Twilio::TwiML::Response to support .to_s and .to_xml
+module Twilio
+  module TwiML
+    class Response
+      def to_s
+        return self.text
+      end
+      def to_xml( options = nil )
+        return self.text
+      end
+    end
+  end
+end
