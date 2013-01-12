550b78773678c775c39a627f92a73ebbf3b6fca4
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-01-12 10:19:00 +0000

    Enhanced errors
    Enhancing errors to include chaining of errors

--- app/classes/ticketplease_errors.rb
+++ app/classes/ticketplease_errors.rb
@@ -1,20 +1,48 @@
 module Ticketplease
 
 class TicketpleaseError < StandardError
+  attr_accessor :original
   attr_accessor :code
   
-  def initialize( msg = nil, code = nil )
+  def initialize( msg = nil, original = nil, code = nil )
     super( msg )
+    self.original = original
     self.code = code
   end
 
 end
 
 class AccountNotAssociatedError < TicketpleaseError
-  def initialize()
-    super( 'Account not associated to object' )
+  def initialize( original = nil, code = nil )
+    super( 'Account not associated to object.', original, code )
   end
 end
 
+class GenericTicketError < TicketpleaseError
+  attr_accessor :ticket
+  def initialize( msg, ticket, original = nil, code = nil )
+    super( msg, original, code )
+    self.ticket = ticket
+  end
+end
+
+class TicketSendingError < GenericTicketError
+  def initialize(ticket, original = nil, code = nil)
+    super( 'Ticket encountered an error while sending.', ticket, original, code )
+  end
+end
+
+class InvalidTicketStateError < GenericTicketError
+  def initialize(ticket, original = nil, code = nil)
+    super( 'The ticket is in an invalid state.', ticket, original, code )
+    self.ticket = ticket
+  end
+end
+
+class MessageAlreadySentError < GenericTicketError
+  def initialize(ticket, original = nil, code = nil)
+    super( 'The ticket message has already been sent. Use force_resend to resend the message.', ticket, original, code )
+  end
+end
 
 end
