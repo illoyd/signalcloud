414bec4105fe136116def9e33866f21f14487c9c
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-01-12 10:19:44 +0000

    Message: Add callback_payload
    Add new column to hold 'callback payload', instead of replacing the
    original payload.

--- db/migrate/20121215135512_create_messages.rb
+++ db/migrate/20121215135512_create_messages.rb
@@ -2,10 +2,11 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.references :ticket, null: false
-      t.string :twilio_sid, null: false, limit: Twilio::SID_LENGTH
+      t.string :twilio_sid, limit: Twilio::SID_LENGTH
       t.decimal :provider_cost, null: false, default: 0, precision: 6, scale: 4
       t.decimal :our_cost, null: false, default: 0, precision: 6, scale: 4
-      t.text :encrypted_payload, null: false
+      t.text :encrypted_payload
+      t.text :encrypted_callback_payload
 
       t.timestamps
     end
