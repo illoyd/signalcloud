e8e46652f38bf6293e01b55cf5b56f3e05d29193
Ian Lloyd <ian.w.lloyd@me.com>
2013-01-22 14:53:24 +0000

    Account & Plan updates

--- db/migrate/20121215134544_create_account_plans.rb
+++ db/migrate/20121215134544_create_account_plans.rb
@@ -3,6 +3,7 @@ class CreateAccountPlans < ActiveRecord::Migration
     create_table :account_plans do |t|
       t.string :label, null: false
       t.boolean :default, null: false, default: false
+      t.integer :plan_kind, null: false, default: AccountPlan::CREDIT, limit: 1
       t.decimal :month, null: false, default: 0, precision: 6, scale: 4
       t.decimal :phone_add, null: false, default: 0, precision: 6, scale: 4
       t.decimal :phone_mult, null: false, default: 0, precision: 6, scale: 4
