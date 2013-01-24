cd7b118251c699dd3ca7c1aab8f9af49a5c36868
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-01-24 07:14:21 +0000

    Twilio routing patches
    Update and create new Twilio routes and tests.

--- spec/routing/twilio_inbound_calls_spec.rb
+++ spec/routing/twilio_inbound_calls_spec.rb
@@ -4,7 +4,11 @@ describe 'routing to twilio/inbound_call' do
   #fixtures :accounts, :account_plans, :users
 
   it 'routes POST twilio/inbound_call to twilio::inbound_call#create' do
-    expect{ post('twilio/inbound_call.xml' }.to route_to( controller: 'twilio/inbound_calls', action: 'create', format: 'xml' )
-  end  
+    expect( post('twilio/inbound_call.xml') ).to route_to( controller: 'twilio/inbound_calls', action: 'create', format: 'xml' )
+  end 
+  
+  it 'cannout route GET twilio/inbound_call' do
+    expect( get('twilio/inbound_call.xml') ).to_not be_routable
+  end
 
 end
