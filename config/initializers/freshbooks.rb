0e58b299299783180baa2c59f2483e28e985792c
Ian Lloyd <ian.w.lloyd@me.com>
2013-01-24 14:36:19 +0000

    Freshbooks account caching
    Added account caching control for FB to speed it up

--- config/initializers/freshbooks.rb
+++ config/initializers/freshbooks.rb
@@ -5,7 +5,7 @@ module Freshbooks
   # Request the current account; expects +ENV['FRESHBOOKS_URL']+ and +ENV['FRESHBOOKS_API_TOKEN']+
   # to be defined in the environment.
   def self.account
-    FreshBooks::Client.new( ENV['FRESHBOOKS_URL'], ENV['FRESHBOOKS_API_TOKEN'] )
+    @@client ||= FreshBooks::Client.new( ENV['FRESHBOOKS_URL'], ENV['FRESHBOOKS_API_TOKEN'] )
   end
 end
  
