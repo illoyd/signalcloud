bcccd77c700aaa487a3085d0ef682dde5272b2fd
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-01-25 18:26:50 +0000

    Add auth helper
    Add authenticate_with_http_digest helper method to support signing into
    the API classes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,9 +40,36 @@ RSpec.configure do |config|
   config.order = "random"
 end
 
-#VCR config
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.default_cassette_options = { :record => :new_episodes, match_requests_on: [:method, :uri, :query, :body] }
   c.hook_into :webmock
+end
+
+##
+# Authenticate helper
+class ActionController::TestCase
+  require 'digest/md5'
+  include Devise::TestHelpers
+
+  def authenticate_with_http_digest(user = nil, password = nil, realm = nil)
+    ActionController::Base.class_eval { include ActionController::Testing }
+
+    @controller.instance_eval %Q(
+      alias real_process_with_new_base_test process_with_new_base_test
+
+      def process_with_new_base_test(request, response)
+        credentials = {
+	  :uri => request.url,
+	  :realm => "#{realm}",
+	  :username => "#{user}",
+	  :nonce => ActionController::HttpAuthentication::Digest.nonce(request.env['action_dispatch.secret_token']),
+	  :opaque => ActionController::HttpAuthentication::Digest.opaque(request.env['action_dispatch.secret_token'])
+        }
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Digest.encode_credentials(request.request_method, credentials, "#{password}", false)
+
+        real_process_with_new_base_test(request, response)
+      end
+    )
+  end
 end
