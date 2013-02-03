501e9813d13b3e790231f2a7f6c6af76b1cf1b47
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-02-03 20:25:51 +0000

    PhoneDirectory tests

--- app/models/phone_directory.rb
+++ app/models/phone_directory.rb
@@ -4,28 +4,59 @@ class PhoneDirectory < ActiveRecord::Base
   belongs_to :account, inverse_of: :phone_directories
   has_many :appliances, inverse_of: :phone_directory
   has_many :phone_directory_entries, inverse_of: :phone_directory, :order => 'country'
+  has_many :phone_numbers, through: :phone_directory_entries #, select: 'phone_numbers.*, phone_directory_entries.country AS country'
+  
+  def phone_numbers_by_country( country )
+    self.phone_numbers.where( 'phone_directory_entries.country' => country )
+  end
+  
+  def default_phone_numbers()
+    self.phone_numbers_by_country(nil)
+  end
+  
+  def country_for_number( number )
+    return case
+      when PhoneTools.united_states?(number)
+        PhoneDirectoryEntry::US
+      when PhoneTools.canadian?(number)
+        PhoneDirectoryEntry::CA
+      when PhoneTools.united_kingdom?(number)
+        PhoneDirectoryEntry::GB
+      else
+        PhoneDirectoryEntry::DEFAULT
+    end
+  end
   
   def select_from_number( to_number )
     # Attempt to map the given number into a country
     components = PhoneTools.componentize to_number
 
     # Read first component - this is usually the country code
-    country = case components.first
+    country = case components.first.to_i
       # For North American numbers, look at second component
       when PhoneTools::NANP_CODE
-        PhoneDirectoryEntry::DEFAULT if PhoneTools.other_nanp_country?(components.second)
-        PhoneTools.canadian?(components.second) ? PhoneDirectoryEntry::CA : PhoneDirectoryEntry::US
+        if PhoneTools.other_nanp_country?(components.second.to_i)
+          PhoneDirectoryEntry::DEFAULT
+        elsif PhoneTools.canadian?(components.second.to_i)
+          PhoneDirectoryEntry::CA
+        else
+          PhoneDirectoryEntry::US
+        end
       when PhoneTools::GB_CODE
         PhoneDirectoryEntry::GB
       else
         PhoneDirectoryEntry::DEFAULT
     end
+    
+    country = self.country_for_number to_number
 
     # Return a random entry from the given country set
+    # Tests for requested country, then nil country, then finally the first number available
+    #puts 'country: %s; array: %s' % [ country, components ]
     phone_number_ids = self.phone_directory_entries.where( country: country ).pluck(:phone_number_id)
     phone_number_ids = self.phone_directory_entries.where( country: nil ).pluck(:phone_number_id) if phone_number_ids.empty?
-    phone_number_ids = self.phone_directory_entries.first if phone_number_ids.empty?
-    PhoneNumber.find(phone_number_ids.sample).number
+    return self.phone_directory_entries.first.phone_number if phone_number_ids.empty?
+    return PhoneNumber.find(phone_number_ids.sample)
   end
 
 end
---
+++ spec/models/phone_directory_spec.rb
@@ -0,0 +1,253 @@
+require 'spec_helper'
+
+describe PhoneDirectory do
+
+  # Construct a directory, attaching numbers for each country as well as defaults
+  def create_directory( defaults=0, us_numbers=0, ca_numbers=0, gb_numbers=0 )
+    directory = create(:phone_directory)
+    
+    defaults.times do
+      pn = create :phone_number, account: directory.account
+      directory.phone_directory_entries.create phone_number_id: pn.id, country: nil
+    end
+    
+    us_numbers.times do
+      pn = create :us_phone_number, account: directory.account
+      directory.phone_directory_entries.create phone_number_id: pn.id, country: PhoneDirectoryEntry::US
+    end
+    
+    ca_numbers.times do
+      pn = create :ca_phone_number, account: directory.account
+      directory.phone_directory_entries.create phone_number_id: pn.id, country: PhoneDirectoryEntry::CA
+    end
+    
+    gb_numbers.times do
+      pn = create :uk_phone_number, account: directory.account
+      directory.phone_directory_entries.create phone_number_id: pn.id, country: PhoneDirectoryEntry::GB
+    end
+    
+    directory.reload
+    directory.phone_directory_entries(true)
+    return directory
+  end
+  
+  let(:us_number) { '+12151234567' }
+  let(:gb_number) { '+447540123456' }
+  let(:ca_number) { '+14161234567' }
+  let(:tt_number) { '+18681234567' }
+  let(:my_number) { '+6012345678' }
+  let(:au_number) { '+61418123456' }
+  let(:ru_number) { '+79111234567' }
+  let(:hk_number) { '+85212345678' }
+  
+  describe '#country_for_number' do
+    let(:directory) { create(:phone_directory) }
+    it 'recognises United States' do
+      directory.country_for_number(us_number).should == PhoneDirectoryEntry::US
+    end
+    it 'recognises Canadian' do
+      directory.country_for_number(ca_number).should == PhoneDirectoryEntry::CA
+    end
+    it 'recognises United Kingdom' do
+      directory.country_for_number(gb_number).should == PhoneDirectoryEntry::GB
+    end
+    
+    it "does not recognise other countries" do
+      { Trinidad: tt_number, Malaysia: my_number, Australia: au_number, Russia: ru_number, HongKong: hk_number }.each do |country,number|
+        directory.country_for_number(number).should == PhoneDirectoryEntry::DEFAULT
+      end
+    end
+  end
+
+  describe '#select_from_number' do
+    let(:default_numbers) { directory.phone_numbers_by_country(nil).pluck('phone_numbers.id') }
+    let(:us_numbers)      { directory.phone_numbers_by_country(PhoneDirectoryEntry::US).pluck('phone_numbers.id') }
+    let(:ca_numbers)      { directory.phone_numbers_by_country(PhoneDirectoryEntry::CA).pluck('phone_numbers.id') }
+    let(:gb_numbers)      { directory.phone_numbers_by_country(PhoneDirectoryEntry::GB).pluck('phone_numbers.id') }
+
+    context 'when using only default numbers' do
+      let(:directory) { create_directory(2, 0, 0, 0) }
+
+      it 'selects a default number for US number' do
+        default_numbers.should include( directory.select_from_number( us_number ).id )
+      end
+      
+      it 'selects a default number for a Canadian number' do
+        default_numbers.should include( directory.select_from_number( ca_number ).id )
+      end
+      
+      it 'selects a default number for a United Kingdom number' do
+        default_numbers.should include( directory.select_from_number( gb_number ).id )
+      end
+      
+      it 'selects a default number for a Trinidad number' do
+        default_numbers.should include( directory.select_from_number( tt_number ).id )
+      end
+      
+      it 'selects a default number for a Malaysian number' do
+        default_numbers.should include( directory.select_from_number( my_number ).id )
+      end
+      
+      it 'selects a default number for a Hong Kong number' do
+        default_numbers.should include( directory.select_from_number( hk_number ).id )
+      end
+      
+      it 'selects a default number for a Australian number' do
+        default_numbers.should include( directory.select_from_number( au_number ).id )
+      end
+      
+      it 'selects a default number for a Russian number' do
+        default_numbers.should include( directory.select_from_number( ru_number ).id )
+      end
+    end
+
+    context 'when using only US numbers' do
+      let(:directory) { create_directory(0, 2, 0, 0) }
+
+      it 'selects a US number for US number' do
+        us_numbers.should include( directory.select_from_number( us_number ).id )
+      end
+      
+      it 'selects a US number for a Canadian number' do
+        us_numbers.should include( directory.select_from_number( ca_number ).id )
+      end
+      
+      it 'selects a US number for a United Kingdom number' do
+        us_numbers.should include( directory.select_from_number( gb_number ).id )
+      end
+      
+      it 'selects a US number for a Trinidad number' do
+        us_numbers.should include( directory.select_from_number( tt_number ).id )
+      end
+      
+      it 'selects a US number for a Malaysian number' do
+        us_numbers.should include( directory.select_from_number( my_number ).id )
+      end
+      
+      it 'selects a US number for a Hong Kong number' do
+        us_numbers.should include( directory.select_from_number( hk_number ).id )
+      end
+      
+      it 'selects a US number for a Australian number' do
+        us_numbers.should include( directory.select_from_number( au_number ).id )
+      end
+      
+      it 'selects a US number for a Russian number' do
+        us_numbers.should include( directory.select_from_number( ru_number ).id )
+      end
+    end
+
+    context 'when using only CA numbers' do
+      let(:directory) { create_directory(0, 0, 2, 0) }
+
+      it 'selects a CA number for US number' do
+        ca_numbers.should include( directory.select_from_number( us_number ).id )
+      end
+      
+      it 'selects a CA number for a Canadian number' do
+        ca_numbers.should include( directory.select_from_number( ca_number ).id )
+      end
+      
+      it 'selects a CA number for a United Kingdom number' do
+        ca_numbers.should include( directory.select_from_number( gb_number ).id )
+      end
+      
+      it 'selects a CA number for a Trinidad number' do
+        ca_numbers.should include( directory.select_from_number( tt_number ).id )
+      end
+      
+      it 'selects a CA number for a Malaysian number' do
+        ca_numbers.should include( directory.select_from_number( my_number ).id )
+      end
+      
+      it 'selects a CA number for a Hong Kong number' do
+        ca_numbers.should include( directory.select_from_number( hk_number ).id )
+      end
+      
+      it 'selects a CA number for a Australian number' do
+        ca_numbers.should include( directory.select_from_number( au_number ).id )
+      end
+      
+      it 'selects a CA number for a Russian number' do
+        ca_numbers.should include( directory.select_from_number( ru_number ).id )
+      end
+    end
+
+    context 'when using only UK numbers' do
+      let(:directory) { create_directory(0, 0, 0, 2) }
+
+      it 'selects a UK number for US number' do
+        gb_numbers.should include( directory.select_from_number( us_number ).id )
+      end
+      
+      it 'selects a UK number for a Canadian number' do
+        gb_numbers.should include( directory.select_from_number( ca_number ).id )
+      end
+      
+      it 'selects a UK number for a United Kingdom number' do
+        gb_numbers.should include( directory.select_from_number( gb_number ).id )
+      end
+      
+      it 'selects a UK number for a Trinidad number' do
+        gb_numbers.should include( directory.select_from_number( tt_number ).id )
+      end
+      
+      it 'selects a UK number for a Malaysian number' do
+        gb_numbers.should include( directory.select_from_number( my_number ).id )
+      end
+      
+      it 'selects a UK number for a Hong Kong number' do
+        gb_numbers.should include( directory.select_from_number( hk_number ).id )
+      end
+      
+      it 'selects a UK number for a Australian number' do
+        gb_numbers.should include( directory.select_from_number( au_number ).id )
+      end
+      
+      it 'selects a UK number for a Russian number' do
+        gb_numbers.should include( directory.select_from_number( ru_number ).id )
+      end
+    end
+
+    context 'when using all numbers' do
+      let(:directory) { create_directory(2, 2, 2, 2) }
+
+      it 'selects a US number for US number' do
+        us_numbers.should include( directory.select_from_number( us_number ).id )
+      end
+      
+      it 'selects a CA number for a Canadian number' do
+        ca_numbers.should include( directory.select_from_number( ca_number ).id )
+      end
+      
+      it 'selects a UK number for a United Kingdom number' do
+        gb_numbers.should include( directory.select_from_number( gb_number ).id )
+      end
+      
+      it 'selects a default number for a Trinidad number' do
+        directory.phone_directory_entries.where( country: PhoneDirectoryEntry::DEFAULT ).each do |entry|
+          puts '%s: %s' % [ entry.country, entry.phone_number_id ]
+        end
+        default_numbers.should include( directory.select_from_number( tt_number ).id )
+      end
+      
+      it 'selects a default number for a Malaysian number' do
+        default_numbers.should include( directory.select_from_number( my_number ).id )
+      end
+      
+      it 'selects a default number for a Hong Kong number' do
+        default_numbers.should include( directory.select_from_number( hk_number ).id )
+      end
+      
+      it 'selects a default number for a Australian number' do
+        default_numbers.should include( directory.select_from_number( au_number ).id )
+      end
+      
+      it 'selects a default number for a Russian number' do
+        default_numbers.should include( directory.select_from_number( ru_number ).id )
+      end
+    end
+
+  end
+
+end
