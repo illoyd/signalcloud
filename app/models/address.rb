67ac74461927f739ff314f5d08635b8a4706d035
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-02-10 10:56:06 +0000

    Address + Specs

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 class Address < ActiveRecord::Base
-  attr_accessible :line1, :line2, :city, :region, :postcode, :country, :account_id
+  attr_accessible :first_name, :last_name, :email, :work_phone, :line1, :line2, :city, :region, :postcode, :country, :account_id
   belongs_to :account
+  
+  validates_presence_of :first_name, :last_name, :email, :city, :region, :postcode, :country
+  
+  #validates_existence_of :account
 
 end
--- spec/factories/address_factories.rb
+++ spec/factories/address_factories.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
 
   factory :address do
+    first_name  'John Q'
+    last_name   'Public'
+    work_phone  '+12021234567'
+    email       'john.q.public@ticketpleaseapp.com'
     line1       'Test Address'
     line2       nil
     city        'Washington'
@@ -9,6 +13,9 @@ FactoryGirl.define do
     country     'US'
 
     factory :white_house_address do
+      first_name  'Barack'
+      last_name   'Obama'
+      email       'theprez@ticketpleaseapp.com'
       line1       '1600 Pennsylvania Ave NW'
       line2       nil
       city        'Washington'
