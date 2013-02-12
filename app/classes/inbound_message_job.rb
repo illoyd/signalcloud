0c48a99c533f4db37fe25c201fa6dda081ffaf34
Ian Lloyd <ian.w.lloyd@me.com>
2013-02-12 17:08:54 +0000

    continuing to work on inbound message

--- app/classes/inbound_message_job.rb
+++ app/classes/inbound_message_job.rb
@@ -4,6 +4,7 @@
 #   +message_values+: all values as passed by the callback
 #   +quiet+: a flag to indicate if this job should report itself on STDOUT
 #
+# Generally +message_values+ should include :to, :from, and :body.
 # This class is intended for use with Delayed::Job.
 #
 class InboundMessageJob < Struct.new( :message_values )
@@ -12,19 +13,72 @@ class InboundMessageJob < Struct.new( :message_values )
   def perform
     self.standardise_message_values!
     
+    # Find all open tickets
+    open_tickets = applicable_tickets()
+    
+    case open_ticket.count
+      # No open tickets found; treat as an unsolicited message.
+      when 0
+        self.internal_phone_number.record_unsolicited_message({
+          customer_number: self.message_values[:from],
+          body: self.message_values[:body],
+          payload: self.message_values
+        })
+        self.internal_phone_number.send_reply_to_unsolicited_message(self.message_values[:from]) if self.internal_phone_number.should_reply_to_unsolicited_sms?
+      
+      # Only one ticket, so process immediately
+      when 1
+      
+      
+      # More than 1, so scan for possible positive or negative match.
+      else
+    
+    end
+    
     # Get all applicable tickets. If none found, this is an 'unsolicited message'.
-    if self.is_unsolicited_message?
-      self.record_unsolicited_message
-      self.send_reply_to_unsolicited_message if self.internal_phone_number.should_reply_to_unsolicited_sms?
+    if open_tickets.empty?
+    
+    # An open ticket was found.
     else
+    
+      # If only one ticket, process like normal
       self.handle_response_to_ticket()
     end
+  end
+
+  ##  
+  # Intuit the appropriate ticket based upon the TO and FROM.
+  # In this situation, we SWAP the given TO and FROM, as this is a reply from the user. Tickets are always from: ticketplease, to: the recepient.
+  def applicable_tickets()
+    normalized_to_number = Ticket.normalize_phone_number self.message_values[:to]
+    normalized_from_number = Ticket.normalize_phone_number self.message_values[:from]
+    @applicable_tickets ||= Ticket.where({
+      encrypted_to_number: Ticket.encrypt( :to_number, normalized_from_number ),
+      encrypted_from_number: Ticket.encrypt( :from_number, normalized_to_number ),
+      status: Ticket::CHALLENGE_SENT
+      })
+  end
+  
+  def handle_response_to_ticket()
+    ticket = self.applicable_tickets.first
+    self.add_message_to_ticket(ticket)
+    self.update_ticket(ticket)
+    ticket.send_reply_message!()
   end
 
   def is_unsolicited_message?
     self.applicable_tickets.empty?
   end
   
+  def internal_phone_number
+    normalized_to_number = Ticket.normalize_phone_number self.message_values[:to]
+    @phone_number ||= PhoneNumber.where( encrypted_to_number: Ticket.encrypt( :to_number, normalized_from_number ) ).first
+  end
+  
+
+
+
+
   def record_unsolicited_message()
     message_status = self.internal_phone_number.account.twilio_account.sms.messages.get( self.message_values[:sms_sid] )
     ledger_entry = self.internal_phone_number.ledger_entries.create( narrative: LedgerEntry::UNSOLICITED_SMS_NARRATIVE, price: message_status.price, notes: self.message_values.to_property_hash )
@@ -40,30 +94,7 @@ class InboundMessageJob < Struct.new( :message_values )
 
 
 
-  def handle_response_to_ticket()
-    ticket = self.applicable_tickets.first
-    self.add_message_to_ticket(ticket)
-    self.update_ticket(ticket)
-    ticket.send_reply_message!()
-  end
   
-  def internal_phone_number
-    normalized_to_number = Ticket.normalize_phone_number self.message_values[:to]
-    @phone_number ||= PhoneNumber.where( encrypted_to_number: Ticket.encrypt( :to_number, normalized_from_number ) ).first
-  end
-  
-  ##  
-  # Intuit the appropriate ticket based upon the TO and FROM.
-  # In this situation, we SWAP the given TO and FROM, as this is a reply from the user. Tickets are always from: ticketplease, to: the recepient.
-  def applicable_tickets()
-    normalized_to_number = Ticket.normalize_phone_number self.message_values[:to]
-    normalized_from_number = Ticket.normalize_phone_number self.message_values[:from]
-    @applicable_tickets ||= Ticket.where({
-      encrypted_to_number: Ticket.encrypt( :to_number, normalized_from_number ),
-      encrypted_from_number: Ticket.encrypt( :from_number, normalized_to_number ),
-      status: Ticket::CHALLENGE_SENT
-      })
-  end
   
   ##
   # Update the ticket...
