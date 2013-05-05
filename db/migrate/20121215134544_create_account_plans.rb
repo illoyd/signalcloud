fc877b2b129fb684cc835e17d2fdd3a79f594f15
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-05-05 22:40:07 +0100

    Updating AccountPlan default calcs
    Adding new phone numbers and a new test stencil

--- db/migrate/20121215134544_create_account_plans.rb
+++ db/migrate/20121215134544_create_account_plans.rb
@@ -6,13 +6,13 @@ class CreateAccountPlans < ActiveRecord::Migration
       t.integer :plan_kind, null: false, default: AccountPlan::CREDIT, limit: 1
       t.decimal :month, null: false, default: 0, precision: 8, scale: 4
       t.decimal :phone_add, null: false, default: 0, precision: 6, scale: 4
-      t.decimal :phone_mult, null: false, default: 1, precision: 6, scale: 4
+      t.decimal :phone_mult, null: false, default: 0, precision: 6, scale: 4
       t.decimal :call_in_add, null: false, default: 0, precision: 6, scale: 4
-      t.decimal :call_in_mult, null: false, default: 1, precision: 6, scale: 4
+      t.decimal :call_in_mult, null: false, default: 0, precision: 6, scale: 4
       t.decimal :sms_in_add, null: false, default: 0, precision: 6, scale: 4
-      t.decimal :sms_in_mult, null: false, default: 1, precision: 6, scale: 4
+      t.decimal :sms_in_mult, null: false, default: 0, precision: 6, scale: 4
       t.decimal :sms_out_add, null: false, default: 0, precision: 6, scale: 4
-      t.decimal :sms_out_mult, null: false, default: 1, precision: 6, scale: 4
+      t.decimal :sms_out_mult, null: false, default: 0, precision: 6, scale: 4
 
       t.timestamps
     end
