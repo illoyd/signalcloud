bacdf543500ae7eba538c5995e4eb4729a44cf0c
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-05-18 16:03:48 +0100

    Gravatar support added!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,4 +65,17 @@ module ApplicationHelper
     html.html_safe
   end
 
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user, size = nil, options={} )
+    gravatar_id = Digest::MD5::hexdigest(user.email.chomp.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url += "?size=#{size}" unless size.nil?
+
+    options = { alt: user.first_name, class: "gravatar" }.merge options
+    options[:height] = size unless size.nil?
+    options[:width] = size unless size.nil?
+
+    image_tag( gravatar_url, options )
+  end
+
 end
