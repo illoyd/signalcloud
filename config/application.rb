14e5e100d1d22844e99de0c252bebad5f1aa42b4
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-09-13 20:28:42 +0100

    Activate strong_parameters

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,7 @@ module SignalCloud
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -76,6 +76,6 @@ module SignalCloud
   end
 end
 
-# ActiveRecord::Base.send(:include, ActiveModel::ForbiddenAttributesProtection)
+ActiveRecord::Base.send(:include, ActiveModel::ForbiddenAttributesProtection)
 
 require './app/classes/exceptions.rb'
