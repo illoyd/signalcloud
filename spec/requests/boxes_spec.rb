90827d48d8e6be8c1b26953f85d5158e5b9c6563
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-10-05 12:07:02 +0100

    Oops

--- spec/requests/boxes_spec.rb
+++ spec/requests/boxes_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Boxes" do
   let!(:user)          { create :user, user_roles: [ UserRole.create(organization: organization, roles: UserRole::ROLES) ] }
   let!(:organization)  { create :organization }
-  let!(:box)           { create :box, organization: organization ) }
+  let!(:box)           { create :box, organization: organization }
   before(:each) { sign_in(user) }
 
   describe "GET /boxes" do
