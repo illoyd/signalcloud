d6ea321c33ba6b6cdb02113ca9c0255586f50d24
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-10-10 20:59:15 +0100

    Editing to boxes

--- db/migrate/20121215135508_create_boxes.rb
+++ db/migrate/20121215135508_create_boxes.rb
@@ -1,8 +1,12 @@
 class CreateBoxes < ActiveRecord::Migration
   def change
     create_table :boxes do |t|
-    
+      t.workflow_state
+      
+      # References
       t.references :organization, null: false
+      
+      # Information
       t.datetime :start_at
       t.string :label
 
