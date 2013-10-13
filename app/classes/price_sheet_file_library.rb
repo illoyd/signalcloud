a87cec7aac241d1f75d674a878870426907a9271
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-10-13 12:33:38 +0100

    Enhance PriceSheetFileLibrary
    To better (de-)serialize price sheets

--- app/classes/price_sheet_file_library.rb
+++ app/classes/price_sheet_file_library.rb
@@ -6,12 +6,18 @@ class PriceSheetFileLibrary
   def price_sheet_for( country )
     filename = filename_for( country )
     raise SignalCloud::UnknownPriceSheetError.new( country ) unless File.exists? filename
-    YAML.load_file filename
+    data = YAML.load_file filename
+    PriceSheet.new( data[:country], data[:base_conversation_price], data[:base_phone_number_price] )
   end
 
   def save( price_sheet )
-    File.open( filename_for(country), 'w' ) do |f|
-      f.write YAML.dump(self)
+    data = {
+      country:                 price_sheet.country.to_s,
+      base_conversation_price: price_sheet.base_conversation_price,
+      base_phone_number_price: price_sheet.base_phone_number_price
+    }
+    File.open( filename_for(price_sheet.country), 'w' ) do |f|
+      f.write YAML.dump(data)
     end
   end
   
