30605f209c7977bbe3a4f7c647ed52e4b09228ea
Ian Lloyd <ian.w.lloyd@gmail.com>
2013-10-19 11:11:36 +0100

    Update box to use conversation workbook reader

--- app/models/box.rb
+++ app/models/box.rb
@@ -3,6 +3,7 @@ class Box < ActiveRecord::Base
   workflow do
     state :draft do
       event :start, transitions_to: :working
+      event :refresh, transitions_to: :draft
     end
     state :working do
       event :complete, transitions_to: :complete
@@ -15,15 +16,31 @@ class Box < ActiveRecord::Base
   
   has_attached_file :document
 
-  validates_presence_of :organization
+  validates :organization, presence: true
   validates :document, attachment_presence: true, attachment_content_type: [ 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ], if: :draft?
   
   protected
   
+  def refresh
+    # Delete all existing draft conversations
+    self.conversations.with_draft_state.clear
+    
+    # Reload from document
+    self.conversations = ConversationWorkbookReader.parse( self.document )
+  end
+  
   def start
+    # Delete the attachment
+    self.document = nil
+  
+    # Initiate new start jobs for all conversations
+    self.conversations.pluck(:id).each do |conversation_id|
+      SendConversationChallengeJob.perform_async( conversation_id )
+    end
   end
   
   def complete
+    # Send a notice that the box is done
   end
   
 end
