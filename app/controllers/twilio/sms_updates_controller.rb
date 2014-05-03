5babd075d228ed986f1ef273becb88e6c13ab3ff
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-05-04 00:25:08 +0100

    Remove actions

--- app/controllers/twilio/sms_updates_controller.rb
+++ app/controllers/twilio/sms_updates_controller.rb
@@ -4,8 +4,8 @@ class Twilio::SmsUpdatesController < Twilio::TwilioController
   # POST /twilio/sms_updates.xml
   def create
     # Build a new update message job using the passed parameters
-    sms = Twilio::MessageStatus.new( params )
-    UpdateMessageStatusJob.perform_async sms.sid, sms.status, nil
+    #sms = Twilio::MessageStatus.new( params )
+    #UpdateMessageStatusJob.perform_async sms.sid, sms.status, nil
     
     # Return a blank response
     render :xml => Twilio::TwiML::Response.new
