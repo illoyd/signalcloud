125662ba72720971d528fbc7b03635e6cde187be
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-05-03 21:23:52 +0100

    Lots of logs in Twilio!

--- app/controllers/twilio/twilio_controller.rb
+++ app/controllers/twilio/twilio_controller.rb
@@ -10,8 +10,11 @@ class Twilio::TwilioController < ApplicationController
   end
 
   def authenticate_twilio!
+    logger.info { 'Twilio: Attempting to authenticate against twilio' }
+
     # If no organization given, kill immediately    
     if @organization.nil? || @organization == false
+      logger.info { 'Twilio: Organisation not recognised' }
       head :unauthorized
 
     # Only continue processing if the organization was found
@@ -21,16 +24,19 @@ class Twilio::TwilioController < ApplicationController
       signature_uri_with_auth = request.original_request_url @organization.sid, @organization.auth_token
       signature_params = request.post? ? request.request_parameters : request.query_parameters
       signature = request.headers.fetch( 'HTTP_X_TWILIO_SIGNATURE', 'NO HEADER GIVEN' )
+      logger.info { "Twilio: Organisation #{ @organization.id } attempted sign-in with #{ signature }" }
       
       # FORBID if does not pass validation
       gateway = @organization.communication_gateway_for :twilio
       unless ( gateway.twilio_validator.validate( signature_uri_without_auth, signature_params, signature ) || gateway.twilio_validator.validate( signature_uri_with_auth, signature_params, signature ) )
         expected_signature_without_auth = gateway.twilio_validator.build_signature_for( signature_uri_without_auth, signature_params )
         expected_signature_with_auth = gateway.twilio_validator.build_signature_for( signature_uri_with_auth, signature_params )
-        logger.error 'Could not auth Twilio ignoring digest! Given %s, expected %s. Using URI %s and POST %s.' % [ signature, expected_signature_without_auth, signature_uri_without_auth, signature_params ]
-        logger.error 'Could not auth Twilio using digest! Given %s, expected %s. Using URI %s and POST %s.' % [ signature, expected_signature_with_auth, signature_uri_with_auth, signature_params ]
+        logger.error 'Twilio: Could not auth Twilio ignoring digest! Given %s, expected %s. Using URI %s and POST %s.' % [ signature, expected_signature_without_auth, signature_uri_without_auth, signature_params ]
+        logger.error 'Twilio: Could not auth Twilio using digest! Given %s, expected %s. Using URI %s and POST %s.' % [ signature, expected_signature_with_auth, signature_uri_with_auth, signature_params ]
         head :forbidden
       end
+
+      logger.info { "Twilio: Organisation #{ @organization.id } fully authenticated." }
     end
   end
 
