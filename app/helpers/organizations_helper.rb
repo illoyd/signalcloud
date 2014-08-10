6d7d1aea905cf649c611aa92ac5958f01ea65fad
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-10 22:24:28 +0100

    Remove Org conversation_status_breakdown

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,12 +1,3 @@
 module OrganizationsHelper
 
-  def conversation_status_breakdown( counts )
-    counts = Conversation.count_by_status_hash(counts) if counts.is_a? ActiveRecord::Relation
-    total = counts.values.sum().to_f
-    statistics = counts.each_with_object({}) do |(k, v), h|
-      h[k] = v.to_f / total * 100.0
-    end
-    render partial: 'conversation_status_breakdown', locals: { counts: counts, statistics: statistics, total: total }
-  end
-
 end
