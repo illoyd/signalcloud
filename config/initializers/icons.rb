500768886eb13cfbc33379f91ccc92abab56658d
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-16 10:13:24 +0100

    CSS & UI Updates
    To better support new UIs for invoices and ledger entries

--- config/initializers/icons.rb
+++ config/initializers/icons.rb
@@ -2,35 +2,53 @@
 # Icons for icon helper, for global management and access. May move into more efficient library later.
 ICONS = {
   # Objects
-  stencils: 'edit',
-  phone_books: 'book',
-  phone_numbers: 'phone',
-  conversations: 'comments',
-  organizations: 'th-large',
-  users: 'group',
+  stencil:        'edit',
+  stencils:       'edit',
+
+  phone_book:     'book',
+  phone_books:    'book',
+
+  phone_number:   'phone',
+  phone_numbers:  'phone',
+
+  conversation:   'comments',
+  conversations:  'comments',
+
+  organization:   'th-large',
+  organizations:  'th-large',
+
+  user:           'user',
+  users:          'group',
+
+  ledger_entry:   'list-alt',
   ledger_entries: 'list-alt',
-  boxes: 'archive',
+
+  invoice:        'file-text-o',
+  invoices:       'file-text-o',
+
+  box:            'archive',
+  boxes:          'archive',
   
   # Common actions
-  show: 'search',
-  edit: 'pencil',
-  delete: 'trash-o',
+  show:           'search',
+  edit:           'pencil',
+  delete:         'trash-o',
+  disable:        'ban',
+  enable:         'check-circle-o',
   
   # Conversation statuses
-  confirmed: 'check-circle',
-  denied: 'minus-circle',
-  failed: 'times-circle',
-  expired: 'clock-o',
-  pending: 'plus-circle',
-  asking: 'question-circle',
-  asked: 'question-circle',
-  received: 'exclamation-circle',
+  draft:          'file-file-o',
+  pending:        'plus-circle',
+  asking:         'question-circle',
+  asked:          'question-circle',
+  received:       'exclamation-circle',
+  confirmed:      'check-circle',
+  denied:         'minus-circle',
+  failed:         'times-circle',
+  expired:        'clock-o',
   challenge_sent: 'plus-circle',
-  draft: 'file-text',
 
-  error: 'warning-sign',
-  
-  disable: 'ban',
-  enable:  'check-circle-o'
-  
+  # Misc
+  error:          'warning-sign'
+    
 }.with_indifferent_access.freeze
