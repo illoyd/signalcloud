cc0a00e5fc12574f7903cbc08c327174fb5ee678
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-18 21:13:18 +0100

    Removing dead controller code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,13 @@ class UsersController < ApplicationController
   
   respond_to :html, :json, :xml
 
-  # before_filter :cannot_manage_organization_owner_roles, only: [:index]
-
   # Index authorisations
-  load_and_authorize_resource :organization
   before_action :assign_default_user_id, only: :show
+  load_and_authorize_resource :organization
   load_and_authorize_resource through: :organization, shallow: true
   
-  # Other authorisations
-  # load_and_authorize_resource except: [:index]
-
   # GET /organization/1/users
   def index
-    # @user_role = UserRole.new( organization: @organization )
     respond_with @organization, @users
   end
 
@@ -25,31 +19,6 @@ class UsersController < ApplicationController
     respond_with @user
   end
 
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    respond_with @user
-  end
-
-  # GET /users/1/edit
-  def edit
-    respond_with @user
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    flash[:notice] = 'User was successfully created.' if @user.update_attributes(user_params)
-    respond_with @user
-  end
-
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    flash[:notice] = 'User was successfully updated.' if @user.update_attributes(user_params)
-    respond_with @user
-  end
-  
   def assign_default_user_id
     params[:id] = current_user.id if params[:id].blank?
   end
