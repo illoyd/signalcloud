0b1d5b8fceed53b00668dbdd83059311e1273fda
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-18 20:32:40 +0100

    Test Mocked message

--- spec/models/mock_communication_gateway_spec.rb
+++ spec/models/mock_communication_gateway_spec.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
 require 'spec_helper'
 
-describe MockCommunicationGateway, :type => :model do
+describe MockCommunicationGateway, :focus, :type => :model do
 
-  describe MockCommunicationGateway::Message do
-    subject { MockCommunicationGateway::Message.new sid: 'Test', to: 'to', from: 'from', body: 'body', status: Message::SENT_SZ, direction: Message::OUT }
+  describe MockCommunicationGateway::MockMessage do
+    subject { MockCommunicationGateway::MockMessage.new sid: 'Test', to: 'to', from: 'from', body: 'body', status: Message::SENT_SZ, direction: Message::OUT }
     it_behaves_like 'a partner message'
   end
 
