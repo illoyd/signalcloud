33c6478fa18143ceabf4cebdc2db2beada8e75b8
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-18 20:06:53 +0100

    Update ledger entry routing

--- spec/routing/ledger_entries_spec.rb
+++ spec/routing/ledger_entries_spec.rb
@@ -3,15 +3,11 @@ require 'spec_helper'
 describe 'routing to ledger_entries', :type => :routing do
 
   it 'allows indexing' do
-    expect( :get => 'organizations/1/ledger_entries' ).to route_to( controller: 'ledger_entries', action: 'index', organization_id: '1' )
+    expect( :get => 'organizations/1/ledger_entries' ).not_to be_routable
   end
   it 'allows showing' do
     expect( :get => 'organizations/1/ledger_entries/1' ).to route_to( controller: 'ledger_entries', action: 'show', id: '1', organization_id: '1' )
   end
-#   it 'does not allow new' do
-#     pending 'Needs analysis of how to block #show gloming on to "new".'
-#     expect( :get => 'ledger_entries/new' ).not_to be_routable
-#   end
   it 'does not allow creating' do
     expect( :post => 'organizations/1/ledger_entries' ).not_to be_routable
   end
