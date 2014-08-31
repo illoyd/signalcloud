9698a421df406c7fe6035e33716427520c817814
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-08-31 09:59:17 +0100

    Missed a spec

--- spec/mailers/twilio_pricesheet_change_mailer_spec.rb
+++ spec/mailers/twilio_pricesheet_change_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "spec_helper"
 
-RSpec.describe TwilioPricesheetChangeAlert, :type => :mailer do
+RSpec.describe TwilioPricesheetChangeMailer, :type => :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
