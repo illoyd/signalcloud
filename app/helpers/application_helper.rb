95e17cb7f9df97706d7702bc67dc4e952d8a2224
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-01-10 12:09:03 +0000

    Introduce general UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  ##
+  # Create a font-awesome icon
+  def icon( kind = :blank, options = {} )
+    kind = ICONS.fetch(kind, kind.to_s.gsub(/_/, '-'))
+    options[:class] = [ 'fa', "fa-#{kind}", options[:class] ].compact
+    content_tag(:i, '', options)
+  end
+  
+  ##
+  # Prefix a string with an icon
+  def iconify(label, icon, options = {})
+    "#{ icon(icon, options) } #{ label }".strip.html_safe
+  end
+
 end
