e6bfbfcb45f36c984475328ed02d25e004d68b1e
Ian Lloyd <ian.w.lloyd@gmail.com>
2015-01-10 10:42:23 +0000

    Enhance Team/User models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,8 @@
 class Team < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner, inverse_of: :owned_teams, class_name: User
+  has_many :memberships, inverse_of: :team
+  has_many :users, through: :memberships
+  
+  validates :name, presence: true
+  validates :owner, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :lockable, :validatable
+  
+  has_many :memberships, inverse_of: :user
+  has_many :teams, through: :memberships
+  has_many :owned_teams, inverse_of: :owner, as: :owner
 end
